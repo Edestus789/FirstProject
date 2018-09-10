d84d27b4d05c783fbae65577372753d4ae66b842
Edestus <Binikl789@yandex.com>
2018-09-10 12:02:05 +0300

    Basic site with CRUD

---
+++ views/site/_search.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\RobotSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="robot-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'YName') ?>
+
+    <?= $form->field($model, 'SName') ?>
+
+    <?= $form->field($model, 'Discipline') ?>
+
+    <?= $form->field($model, 'Platform') ?>
+
+    <?php // echo $form->field($model, 'Weight') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
